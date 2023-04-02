0eeca5760a2e85a9f0181fcf8f49de4d4012f3fa
arisygdc <arisycso@gmail.com>
2023-04-02 17:01:28 +0700

    inject class

--- Tiketoo/Program.cs
+++ Tiketoo/Program.cs
@@ -1,7 +1,16 @@
+using Tiketoo.Repository.SqlServer;
+using Tiketoo.Services.Tickets;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-var app = builder.Build();
+// inject Ticket service
+builder.Services.AddScoped<ITicketService, TicketService>();
+builder.Services.AddDbContext<TicketRepository>(options => {
+    options.UseSqlServer(builder.Configuration["ConnectionString:SqlServer"]);
+});
 
+var app = builder.Build();
 app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
